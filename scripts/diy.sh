2d2eb693df0e44e6501038f83dc66544634898fc
zongheng23333 <1954640276@qq.com>
2024-06-25 09:02:04 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -3,6 +3,7 @@
 
 
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-./scripts/feeds update
+./scripts/feeds update -a 
 ./scripts/feeds install -a  
 
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
