9fb13434af7afc684df7bbe3bbb1123c98a93658
zongheng23333 <1954640276@qq.com>
2024-08-24 17:32:59 +0800

    Update diy-script.sh

--- scripts/diy-script.sh
+++ scripts/diy-script.sh
@@ -24,8 +24,7 @@ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
 ./scripts/feeds install -a 
 
 
